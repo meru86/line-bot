ad68ee96e125673151a90fd0f15c53f2b1ed5abe
川野晴也 <haru@kawanoharuyanoMacBook-Air.local>
2021-03-04 23:25:08 +0900

    heroku

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http.response import HttpResponse
+from django.http.response import HttpResponse, HttpResponseBadRequest, HttpResponseServerError
 from django.views.generic.base import View
 
 class CallbackView(View):
