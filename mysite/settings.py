bdbaa12910c859aaaba7fca28f2b5cc44dbff2a0
川野晴也 <haru@kawanoharuyanoMacBook-Air.local>
2021-03-05 14:41:30 +0900

    heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'eq7#j)1u2y3^@m()c+-adcg3o#le^tjnt0f22sxu98%+!ox29j'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -127,4 +127,7 @@ except ImportError:
 
 if not DEBUG:
     import django_heroku
+    import os
+    CHANNEL_ACCESS_TOKEN = os.environ["CHANNEL_ACCESS_TOKEN"]
+    CHANNEL_SECRET = os.environ["CHANNEL_SECRET"]
     django_heroku.settings(locals())
